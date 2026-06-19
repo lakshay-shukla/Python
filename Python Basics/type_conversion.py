bb6a2be782bf260e8633f0945b0d795b22703110
Lakshay Shukla <lakshayshukla900@gmail.com>
2026-06-19 11:48:29 +0530

    Added Python Basics

---
+++ Python Basics/type_conversion.py
@@ -0,0 +1,16 @@
+# There are two types of type conversion
+#1 Implicit
+print(5+5.6)
+print(type(5),type(5.6))
+
+# print(4 + '4') # python iss type ke data prr khud se type conversion nahi karta
+
+#2 Explicit
+# str -> int
+#int(4+5j)
+
+# int to str
+str(5)
+
+# float
+float(4)
