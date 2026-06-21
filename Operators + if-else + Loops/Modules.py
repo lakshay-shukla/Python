bd5d596741fdee46705f1705d090d1f56f9e86c4
Lakshay Shukla <lakshayshukla900@gmail.com>
2026-06-21 20:45:45 +0530

    Added Python Basics

---
+++ Operators + if-else + Loops/Modules.py
@@ -0,0 +1,14 @@
+#1. math --> Gives you access to advanced mathematical functions and constants. like find square root, square , LCM
+import math
+print(math.sqrt(196))
+#2. keywords --> Its help you check, track, and list python's reserved keywords
+import keyword
+print(keyword.kwlist)
+#3. random --> it generate random no.'s
+import random
+print(random.randint(1,100))
+#4. datetime --> Used to work with date and time
+import datetime
+print(datetime.datetime.now())
+#5 help --> its for check all modules in python
+help('modules')
