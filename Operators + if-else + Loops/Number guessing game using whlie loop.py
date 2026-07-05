b63b4e14f32f9a1c52da542a6d38338455f3a02c
lakshay-shukla <lakshayshukla900@gmail.com>
2026-07-05 23:22:28 +0530

    Fix formatting of print statements in guessing game

--- Operators + if-else + Loops/Number guessing game using whlie loop.py
+++ Operators + if-else + Loops/Number guessing game using whlie loop.py
@@ -6,9 +6,9 @@ guess = int(input('guess karo: '))
 counter = 1
 while guess != jackpot:
   if guess < jackpot:
-    print('galat!guess higher')
+    print('galat! guess higher')
   else:
-    print('galat!guess lower')
+    print('galat! guess lower')
 
   guess = int(input('guess karo'))
   counter += 1
