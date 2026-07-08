06075b954a4132b6b8d56aac875d4b341a6af496
Lakshay Shukla <lakshayshukla900@gmail.com>
2026-07-08 23:29:40 +0530

    Added Python Basics

--- Operators + if-else + Loops/Nested Loops.py
+++ Operators + if-else + Loops/Nested Loops.py
@@ -4,3 +4,33 @@
 for i in range(1,5):
   for j in range(1,5):
     print(i,j)
+
+
+# Example on Patterns
+#1.
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+rows = int(input('enter number of rows: '))
+for i in range(1,rows+1):
+  for j in range(1,i+1):
+    print('*',end=' ')
+  print()
+
+#2.
+#1
+#121
+#12321
+#1234321
+
+rows = int(input('enter number of rows: '))
+for i in range(1,rows+1):
+  for j in range(1,i+1):
+    print(j,end='')
+  for k in range(i-1,0,-1):
+    print(k,end='')
+
+  print()
