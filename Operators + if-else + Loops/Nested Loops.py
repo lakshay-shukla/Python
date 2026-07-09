2a5cede4ec6b511f5a60327b85e334ab5d1b705c
lakshay-shukla <lakshayshukla900@gmail.com>
2026-07-09 23:05:40 +0530

    Update Nested Loops.py
    Updated the range in nested loops to iterate from 1 to 4.

--- Operators + if-else + Loops/Nested Loops.py
+++ Operators + if-else + Loops/Nested Loops.py
@@ -1,8 +1,8 @@
 # Nested Loop- A nested loop in Python is a loop placed inside another loop.
 
 # Example - # Examples -> unique pairs
-for i in range(1,6):
-  for j in range(1,6):
+for i in range(1,5):
+  for j in range(1,5):
     print(i,j)
 
 
