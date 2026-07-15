3bc88911b5b0cc991c32a8017c714b02ad37793e
Lakshay Shukla <lakshayshukla900@gmail.com>
2026-07-15 23:48:15 +0530

    String Function

---
+++ String/Strings functions.py
@@ -0,0 +1,16 @@
+#1. Common Functions
+# len()
+c = 'kolkata'
+print(len(c))
+# max()
+print(max(c))
+# min()
+print(min(c))
+# sorted()
+print(sorted(c))
+print(sorted(c, reverse=True))
+
+#2. Count function
+print("it is raining".count("i"))
+print("it is raining".count("ing"))
+print("it is raining".count("x"))
