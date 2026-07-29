30c5d1cbb33ffae90460c9967a4e1ec01595eea3
Lakshay Shukla <lakshayshukla900@gmail.com>
2026-07-29 23:53:21 +0530

    Methods to creating tuples

---
+++ Tuples + Sets + Dictionary/Basics of Tuples.py
@@ -0,0 +1,29 @@
+# Tuple --> A tuple in Python is similar to a list. The difference between the two is that we cannot change the elements of a tuple once it is assigned whereas we can change the elements of a list.
+# In short, a tuple is an immutable list. A tuple can not be changed in any way once it is created.
+#Characterstics
+#A. Ordered
+#B. Unchangeble
+#C. Allows duplicate
+
+#1. Creating Tuples
+# Empty
+t1 = ()
+print(t1)  # ()
+# Create a tuple with a single item
+t2 = ('hello',)
+print(t2)  # ('hello',)
+print(type(t2))  # <class 'tuple'>
+# Homogenous
+t3 = (1,2,3,4)
+print(t3)  # (1, 2, 3, 4)
+# Hetrogenous
+t4 = (1,2.5,True,[1,2,3])
+print(t4)  # (1, 2.5, True, [1, 2, 3])
+# Tuple
+t5 = (1,2,3,(4,5))
+print(t5)   #  (1, 2, 3, (4, 5))
+# Using type conversion
+t6 = tuple('hello')
+print(t6)  # ('h', 'e', 'l', 'l', 'o')
+
+#2. 
