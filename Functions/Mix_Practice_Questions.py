a69329486f288c5d7322c309fee0350306ecbf2c
Lakshay Shukla <lakshayshukla900@gmail.com>
2026-08-19 23:13:38 +0530

    List, Dictionary, Loops and Conditional Logic

---
+++ Functions/Mix_Practice_Questions.py
@@ -0,0 +1,28 @@
+# Q1: List Filtering
+# Given a list of salaries: salaries = [25000, 45000, 15000, 60000, 30000]
+# Create a new list containing only the salaries that are strictly greater than 35000.
+salaries = [25000, 45000, 15000, 60000, 30000]
+new_salary = [ ]
+for i in salaries:
+    if i > 35000:
+        new_salary.append(i)
+print(new_salary)
+
+# Q2: Dictionary Manipulation
+# Given a dictionary: user = {"name": "Aman", "age": 24, "city": "Delhi"}
+# Add a new key-value pair {"is_active": True} and update the value of "city" to "Bangalore".
+user = {"name": "Aman", "age": 24, "city": "Delhi"}
+user["city"] = "bangalore"
+user["is_active"] = True
+print(user)
+
+# Q3: Loops and Conditional Logic (Missing Data Handling)
+# Given a list of data points: data = [10, 25, -1, 40, -1, 55]
+# Iterate through the list using a for loop. If the value is -1, print "Missing Data".
+# Otherwise, print "Valid Data: [value]".
+data = [10, 25, -1, 40, -1, 55]
+for i in data:
+    if i == -1:
+        print('Missing Data')
+    else:
+        print('Valid Data: ', [i])
